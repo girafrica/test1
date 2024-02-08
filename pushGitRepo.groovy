baa1066f14de30518e96da7c1394c9e0b68ca3b0
Alexander Davydov <Alexander_Davydov@epam.com>
2024-02-08 15:25:36 +0300

    c
    c

--- pushGitRepo.groovy
+++ pushGitRepo.groovy
@@ -1,6 +1,12 @@
-def call() {
-    withCredentials([usernamePassword(credentialsId: 'github-app', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
-        sh("git tag -a some_tag -m 'Jenkins'")
-        sh('git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/girafrica/tag --tags')
-    }
+pipeline {
+    agent none
+    
+        stage('Build Image') {
+            steps {
+                withCredentials([usernamePassword(credentialsId: 'github-app', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
+                sh("git tag -a some_tag -m 'Jenkins'")
+                sh('git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/girafrica/tag --tags')              
+          }
+            }
+        }
 }
