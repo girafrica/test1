0893207bce0ab2af7c917190ddb51ce6441a7300
Alexander Davydov <Alexander_Davydov@epam.com>
2024-02-08 15:53:49 +0300

    d

--- pushGitRepo.groovy
+++ pushGitRepo.groovy
@@ -5,14 +5,16 @@ pipeline {
     stages {
         stage('Create tag') {
             steps {
-                def date = new Date()
-                sdf = new SimpleDateFormat("dd-MM-yyyy")
-                println("Date is: "+sdf.format(date))
-                def TAG="tag-${sdf.format(date)}"
-                echo "TAG is : ${TAG}"
-                withCredentials([usernamePassword(credentialsId: 'github-app', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
-                sh("git tag -a ${TAG} -m '${TAG}'")
-                sh('git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/girafrica/tag --tags')              
+                script {
+                    def date = new Date()
+                    sdf = new SimpleDateFormat("dd-MM-yyyy")
+                    println("Date is: "+sdf.format(date))
+                    def TAG="tag-${sdf.format(date)}"
+                    echo "TAG is : ${TAG}"
+                    withCredentials([usernamePassword(credentialsId: 'github-app', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
+                    sh("git tag -a ${TAG} -m '${TAG}'")
+                    sh('git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/girafrica/tag --tags')              
+                    }
                 }
             }
         }
