cd3f9c1869d056aada7bc8961c1caeeaea8a1065
Alexander Davydov <Alexander_Davydov@epam.com>
2024-02-08 16:54:25 +0300

    s

--- pushGitRepo.groovy
+++ pushGitRepo.groovy
@@ -37,6 +37,8 @@ pipeline {
                   lastTag = sh script: """git tag --list ${version}* --sort=-version:refname | sort -r | head -1 | grep -oE '[0-9]+\044'""".trim(), returnStdout: true
                   lt = lastTag.trim()  // the .trim() is necessary
                   echo "lastTag: " + lt
+
+                  echo newtag = lt + 1
                 }
             }
         }  
