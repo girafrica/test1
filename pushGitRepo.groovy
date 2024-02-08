c20ffc7d4be73474992bb8f24c40f91fa0954ad1
Alexander Davydov <Alexander_Davydov@epam.com>
2024-02-08 15:10:22 +0300

    d
    d

--- pushGitRepo.groovy
+++ pushGitRepo.groovy
@@ -1,4 +1,4 @@
-withCredentials([usernamePassword(credentialsId: 'git-pass-credentials-ID', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
+withCredentials([usernamePassword(credentialsId: '821808', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
     sh("git tag -a some_tag -m 'Jenkins'")
     sh('git push https://${GIT_USERNAME}:${GIT_PASSWORD}@<REPO> --tags')
 }
