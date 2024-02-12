3fee060920479a76b4c93c64cf9c0f2183c9a058
Alexander Davydov <Alexander_Davydov@epam.com>
2024-02-12 15:40:12 +0300

    d

--- pushGitRepo.groovy
+++ pushGitRepo.groovy
@@ -57,10 +57,7 @@ pipeline {
         stage('Create tag') {
             steps {
                 script {
-                    withCredentials([usernamePassword(credentialsId: 'github-app', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
-                    sh("git tag -f -a ${version}.${newtag} -m '${version}.${newtag}'")
-                    sh('git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/girafrica/tag --tags -f')
-                    }
+                    createTag(version, newtag)            
                 }
             }
         }
