ac2a71c00028830b1d8eb3447f2ca9aa781f93c3
Alexander Davydov <Alexander_Davydov@epam.com>
2024-02-13 14:26:02 +0300

    /

--- pushGitRepo.groovy
+++ pushGitRepo.groovy
@@ -65,9 +65,8 @@ pipeline {
                         dir ('foo'){
                             sh (' ls -l ')                    
                             sh (' git config --global pull.rebase false ')
-                            sh (' git fetch https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/girafrica/release-tags ')
-                            sh (' git rebase --skip ')
-                            sh (' git pull https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/girafrica/release-tags --rebase')
+                            //sh (' git fetch https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/girafrica/release-tags ')
+                            sh (' git pull https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/girafrica/release-tags ')
                             def readContent = "${version}.sbt"
                             writeFile file: "${version}.sbt", text: readContent+"\r\nversion := 1.0.${env.BUILD_ID}"
                             sh (" git add -A")
