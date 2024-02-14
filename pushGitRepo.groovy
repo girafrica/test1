506fdbf63e8327f5cf6944c379f676b9ba594a74
Alexander Davydov <Alexander_Davydov@epam.com>
2024-02-14 14:49:48 +0300

    s

--- pushGitRepo.groovy
+++ pushGitRepo.groovy
@@ -6,29 +6,7 @@ pipeline {
     options {
         skipDefaultCheckout()
     }
-    stages {
-        // stage('Checkout') {
-        //     steps {
-        //         checkout scm
-        //     }
-        // }
-
-        // stage('Checkout2') {
-        //     steps {
-        //         script {
-        //             checkoutCode()              
-        //             }                
-        //         }            
-        //     }
-
-        stage('Create version') {
-            steps {
-                script {
-                    currentDateTime = sh script: """date +"v%Y.%V" """.trim(), returnStdout: true
-                    version = currentDateTime.trim()  // the .trim() is necessary
-                }
-            }
-        }    
+    stages {  
 
         // stage('List tags') {
         //     steps {
@@ -58,6 +36,8 @@ pipeline {
         stage('Create tag') {
             steps {
                 script {
+                    currentDateTime = sh script: """date +"v%Y.%V" """.trim(), returnStdout: true
+                    version = currentDateTime.trim()  // the .trim() is necessary
                     createTag(version)
                 }
             }
@@ -66,7 +46,11 @@ pipeline {
         stage('Save tag') {
             steps {
                 script {
-                    withCredentials([usernamePassword(credentialsId: 'github-app', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
+
+                    savetag()     
+
+
+                    ////withCredentials([usernamePassword(credentialsId: 'github-app', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
                         // dir ('foo'){
                         //     sh (' ls -l ')
                         //     //sh (' git config --global pull.rebase false ')
@@ -80,24 +64,25 @@ pipeline {
                         //     sh (' git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/girafrica/release-tags HEAD:main')
                         // }
 
-                        cloneToLocation("https://github.com/girafrica/release-tags", 'github-app')
+                        ////cloneToLocation("https://github.com/girafrica/release-tags", 'github-app')
+                        
                         //def readContent = "${version}.${newtag}"
                         // def newtag = "${env.BUILD_ID}"
                         // tag = newtag.toString()
 
                         // echo tag 
 
-                        if (!fileExists('releases')) {
-                            writeFile file: 'releases', text: "Releases:"
-                        }
+                        // if (!fileExists('releases')) {
+                        //     writeFile file: 'releases', text: "Releases:"
+                        // }
 
-                        def readContent = readFile 'releases'
+                        // def readContent = readFile 'releases'
 
-                        writeFile file: 'releases', text: readContent+"\r\n${version}.${env.BUILD_ID}"
-                        sh (" git add -A")
-                        sh (" git commit -am 'Updated version number to ${version}.${env.BUILD_ID}'")
-                        sh (' git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/girafrica/release-tags HEAD:main')
-                    }
+                        // writeFile file: 'releases', text: readContent+"\r\n${version}.${env.BUILD_ID}"
+                        // sh (" git add -A")
+                        // sh (" git commit -am 'Updated version number to ${version}.${env.BUILD_ID}'")
+                        // sh (' git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/girafrica/release-tags HEAD:main')
+                    ////}
                 }
             }
         }
